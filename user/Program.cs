b3eaca003f601726145cecd2de864e07445eb641
Ali Gökçay <124869675+aligokcay@users.noreply.github.com>
2025-06-25 13:58:29 +0300

    first commit ali

---
+++ user/Program.cs
@@ -0,0 +1,28 @@
+using trendyolApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddDbContext<AppDbContext>(options => // EF Core’un AppDbContext sınıfını servislere tanıtır, böylece veritabanına erişim sağlar.
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));  // postgreSQL ***
+
+builder.Services.AddControllers(); // RESTful API'de klasik controller yapısını kullanabilmek için gerekli altyapıyı sağlar.
+builder.Services.AddEndpointsApiExplorer(); // Swagger dokümantasyonu için gerekli servisleri etkinleştirir.
+builder.Services.AddSwaggerGen(); // Swagger arayüzü (UI) için gerekli yapılandırmayı ekler.
+
+
+var app = builder.Build(); // Uygulamayı başlatmak için builder nesnesinden bir app nesnesi oluşturur.
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(); // klasik Swagger yapısı
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection(); // HTTP isteklerini otomatik olarak HTTPS’e yönlendirir.
+app.MapControllers();
+
+app.Run();
