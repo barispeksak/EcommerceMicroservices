40707f1a94bc8230e798113b1623761085e9e9bd
Fatma Akyıldız <142047815+fatmakyldz@users.noreply.github.com>
2025-06-27 10:05:02 +0300

    Delete ProductMicroservice directory

--- ProductMicroservice/DTOs/ProductConfigurationDto.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace ProductService.DTOs;
-
-public class ProductConfigurationDto
-{
-    [Required] public int ProductItemId { get; set; }
-    [Required] public int VariationOptionId { get; set; }
-}
--- ProductMicroservice/DTOs/VariationOptionDto.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-namespace ProductService.DTOs;
-
-public class VariationOptionDto
-{
-    [Required] public string Value { get; set; } = null!;
-    [Required] public int VariationId { get; set; }
-}
