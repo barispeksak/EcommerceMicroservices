a09e38c098d1cb234bfd2702924b16ae1423d813
Ali Gökçay <124869675+aligokcay@users.noreply.github.com>
2025-06-30 15:29:50 +0300

    Delete ShippingTypeMicroservice/Data/Entities/UserAddress.cs

--- ShippingTypeMicroservice/Data/Entities/UserAddress.cs
+++
@@ -1,8 +0,0 @@
-namespace UserAddressMicroservice.Data.Entities
-{
-    public class UserAddress
-    {
-        public int UserId { get; set; }
-        public int AddressId { get; set; }
-    }
-}
