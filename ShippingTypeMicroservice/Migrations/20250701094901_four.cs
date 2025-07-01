6fa017287c579d049552b7c2bead376d6d9c0001
Ali Gökçay <124869675+aligokcay@users.noreply.github.com>
2025-07-01 15:28:05 +0300

    shipping-type fixed

---
+++ ShippingTypeMicroservice/Migrations/20250701094901_four.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShippingTypeMicroservice.Migrations
+{
+    /// <inheritdoc />
+    public partial class four : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Method",
+                table: "shipping_type",
+                newName: "Type");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "shipping_type",
+                newName: "Method");
+        }
+    }
+}
