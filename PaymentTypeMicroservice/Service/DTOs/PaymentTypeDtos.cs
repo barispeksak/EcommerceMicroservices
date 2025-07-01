3d02e903f9666fabe8e5b7a9f985b2c88a531ced
Ali Gökçay <124869675+aligokcay@users.noreply.github.com>
2025-07-01 11:51:28 +0300

    Update PaymentTypeDtos.cs

--- PaymentTypeMicroservice/Service/DTOs/PaymentTypeDtos.cs
+++ PaymentTypeMicroservice/Service/DTOs/PaymentTypeDtos.cs
@@ -4,20 +4,18 @@ namespace PaymentTypeMicroservice.Data.Dtos
     public class CreatePaymentTypeDto
     {
         public string Type { get; set; } = string.Empty;
-        public decimal Price { get; set; }
+
     }
 
     public class UpdatePaymentTypeDto
     {
         public int Id { get; set; }
         public string Type { get; set; } = string.Empty;
-        public decimal Price { get; set; }
     }
 
     public class PaymentTypeDto
     {
         public int Id { get; set; }
         public string Type { get; set; } = string.Empty;
-        public decimal Price { get; set; }
     }
 }
