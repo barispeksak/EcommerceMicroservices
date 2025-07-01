257429c8924d8b302e4ac8299b660d5922aabd66
Ali Gökçay <124869675+aligokcay@users.noreply.github.com>
2025-07-01 11:47:43 +0300

    Update ShippingType.cs

--- ShippingTypeMicroservice/Models/ShippingType.cs
+++ ShippingTypeMicroservice/Models/ShippingType.cs
@@ -8,6 +8,6 @@ namespace ShippingTypeMicroservice.Models
         public int Id { get; set; }
 
         [Required]
-        public string Method { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
     }
 }
