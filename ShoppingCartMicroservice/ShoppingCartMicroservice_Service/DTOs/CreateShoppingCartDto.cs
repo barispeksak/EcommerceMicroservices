6cf7fa157756323f5b134293ec2ae8c26040214d
Fatma Akyıldız <142047815+fatmakyldz@users.noreply.github.com>
2025-07-02 15:15:25 +0300

    Delete ShoppingCartMicroservice directory

--- ShoppingCartMicroservice/ShoppingCartMicroservice_Service/DTOs/CreateShoppingCartDto.cs
+++
@@ -1,9 +0,0 @@
-namespace ShoppingCartMicroservice_Service.DTOs
-{
-    public class CreateShoppingCartDto
-    {
-        public int CartId        { get; set; } 
-        public int ProductItemId { get; set; }
-        public int Qty { get; set; }
-    }
-}
