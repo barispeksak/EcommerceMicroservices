b4bfd4fe37cca7f44c94fd9422501d3749ec2430
Fatma Akyıldız <fatma.akyildiz162014@gmail.com>
2025-07-07 09:42:52 +0300

    update  ShoppingCartMicroservice

--- ShoppingCartMicroservice/ShoppingCartMicroservice_Service/obj/Debug/net9.0/ShoppingCartMicroservice_Service.AssemblyInfo.cs
+++ ShoppingCartMicroservice/ShoppingCartMicroservice_Service/obj/Debug/net9.0/ShoppingCartMicroservice_Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingCartMicroservice_Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01e95d685719c65249819d9b5d3997c5c9047d13")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef6d062d92d12ee7e512d1e727a8c1a652bdf436")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingCartMicroservice_Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingCartMicroservice_Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
