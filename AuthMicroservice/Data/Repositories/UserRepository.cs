fbef49b5ae1c36f7c10df13bd9f490d2016560cc
Ali Gökçay <124869675+aligokcay@users.noreply.github.com>
2025-07-07 09:35:18 +0300

    Update UserRepository.cs

--- AuthMicroservice/Data/Repositories/UserRepository.cs
+++ AuthMicroservice/Data/Repositories/UserRepository.cs
@@ -35,5 +35,14 @@ namespace AuthMicroservice.Data.Repositories
         {
             return await _context.Users.ToListAsync();
         }
+        public async Task DeleteAsync(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }
